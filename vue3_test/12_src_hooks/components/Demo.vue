84a957f259a102c1aa81e106bc8e80fb12c507be
Limbo <1034935762@qq.com>
2022-02-19 16:56:58 +0800

    2022年2月19日16:56:58

--- vue3_test/12_src_hooks/components/Demo.vue
+++ vue3_test/12_src_hooks/components/Demo.vue
@@ -2,17 +2,21 @@
 	<h2>当前求和为:{{ sum }}</h2>
 	<button @click="sum++">+1</button>
 	<hr />
+	<h2>当前鼠标点击时的坐标为:x:{{ point.x }}, y:{{ point.y }}</h2>
 </template>
 
 <script>
 import { ref, reactive, onMounted } from 'vue'
 
+import usePoint from '../hooks/usePoint'
 export default {
 	name: 'Demo',
 	setup() {
 		let sum = ref(0)
+		let point = usePoint()
 		return {
-			sum
+			sum,
+			point
 		}
 	}
 }
